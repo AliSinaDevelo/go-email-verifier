713cee331eb3c2e5d72607d19da46ae32ad80f52
AliSina Karimi <100498538+AliSinaDevelo@users.noreply.github.com>
2023-12-09 10:56:19 +0100

    basic functionality and packages added

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"strings"
+)
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	fmt.Printf("DOMAIN, hasMX, hasSTF, sprRecords, hasDMARC, dmarcRecord\n")
+
+	for scanner.Scan() {
+		checkDomain(scanner.Text())
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal("Error: CAN'T READ FROM INPUT: %v\n", err)
+	}
+}
+
+func checkDomain(domain string) {
+
+}
